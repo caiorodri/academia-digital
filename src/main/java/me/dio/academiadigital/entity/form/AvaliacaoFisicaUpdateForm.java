99d045165b55df7240795b126a953ebfc3bca348
caiorodri <caiodossantosrodrigues@gmail.com>
2023-04-19 13:56:23 -0300

    Desenvolvimento dos Formulários

--- src/main/java/me/dio/academiadigital/entity/form/AvaliacaoFisicaUpdateForm.java
+++ src/main/java/me/dio/academiadigital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -1,5 +1,12 @@
 package me.dio.academiadigital.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvaliacaoFisicaUpdateForm {
 
   private double peso;
