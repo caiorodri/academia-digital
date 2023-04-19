f8e163e77f4d793b41fb0eb8ae61ee3e3e199e3d
caiorodri <caiodossantosrodrigues@gmail.com>
2023-04-19 13:57:21 -0300

    Criação dos Repositórios

--- src/main/java/me/dio/academiadigital/repository/AvaliacaoFisicaRepository.java
+++ src/main/java/me/dio/academiadigital/repository/AvaliacaoFisicaRepository.java
@@ -1,4 +1,7 @@
 package me.dio.academiadigital.repository;
 
-public interface AvaliacaoFisicaRepository {
+import me.dio.academiadigital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
 }
--- src/main/java/me/dio/academiadigital/repository/MatriculaRepository.java
+++ src/main/java/me/dio/academiadigital/repository/MatriculaRepository.java
@@ -1,4 +1,7 @@
 package me.dio.academiadigital.repository;
 
-public interface MatriculaRepository {
+import me.dio.academiadigital.entity.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
 }
