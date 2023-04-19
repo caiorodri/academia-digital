2391e855a7bdec3cbb70a76a2e26fa995447068a
caiorodri <caiodossantosrodrigues@gmail.com>
2023-04-19 13:55:16 -0300

    Desenvolvimento dos Controllers

--- src/main/java/me/dio/academiadigital/controller/AlunoController.java
+++ src/main/java/me/dio/academiadigital/controller/AlunoController.java
@@ -1,7 +1,10 @@
 package me.dio.academiadigital.controller;
 
+import jakarta.validation.Valid;
 import me.dio.academiadigital.entity.Aluno;
+import me.dio.academiadigital.entity.AvaliacaoFisica;
 import me.dio.academiadigital.entity.form.AlunoForm;
+import me.dio.academiadigital.entity.form.AlunoUpdateForm;
 import me.dio.academiadigital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,13 @@ public class AlunoController {
     @Autowired
     private AlunoServiceImpl service;
 
+    @PostMapping
+    public Aluno create(@Valid @RequestBody AlunoForm form){
+
+        return service.create(form);
+
+    }
+
     @GetMapping
     public List<Aluno> getAll(){
 
@@ -22,13 +32,29 @@ public class AlunoController {
 
     }
 
-    @PostMapping
-    public Aluno create(@RequestBody AlunoForm form){
+    @GetMapping("/{id}")
+    public Aluno getById(@PathVariable Long id){
 
-        return service.create(form);
+        return service.getById(id);
 
     }
 
+    @GetMapping("/{id}/avaliacoes")
+    public List<AvaliacaoFisica> getAllAvaliacoesFisica(@PathVariable Long id){
 
+        return service.getAllAvaliacoesFisica(id);
+
+    }
+
+    @PutMapping
+    public Aluno update(@RequestBody Long id, AlunoUpdateForm form){
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void delete(@PathVariable Long id){
+
+    }
 
 }
